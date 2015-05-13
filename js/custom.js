b57ebd807dd1896fc7d6053b1ab4d20054ef6af6
YIU Man Tung <mtyiu@cse.cuhk.edu.hk>
2015-05-13 13:21:37 +0800

    Update links

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,19 @@
 $( function() {
 	$( '#collapse-all' ).click( function() {
-		$( '.collapse-tut.in' ).collapse( 'hide' );
-	} );
+		$( '.collapse-tut.in' ).collapse( 'hide' )
+	} )
 	$( '#expand-all' ).click( function() {
-		$( '.collapse-tut' ).collapse( 'show' );
-	} );
+		$( '.collapse-tut' ).collapse( 'show' )
+	} )
 	$( '#show-current-only' ).click( function() {
-		$( '.collapse-tut.in:not(.current)' ).collapse( 'hide' );
-		$( '.collapse-tut.current:not(.in)' ).collapse( 'show' );
-	} );
+		$( '.collapse-tut.in:not(.current)' ).collapse( 'hide' )
+		$( '.collapse-tut.current:not(.in)' ).collapse( 'show' )
+	} )
 	$( '#show-all-outline' ).click( function() {
-		$( '.collapse-outline' ).collapse( 'toggle' );
-	} );
-} );
+		$( '.collapse-outline' ).collapse( 'toggle' )
+	} )
+} )
 
 var download = function( url ) {
-	window.open( url, '_blank' );
-	ga( 'send', 'event', 'download', url );
-};
+	ga( 'send', 'event', 'download', url )
+}
